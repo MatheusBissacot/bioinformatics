65568a3b7c87247f7a865675fdc0700851d7907f
Gonçalo Brochado <126338877+GBrochado@users.noreply.github.com>
2025-04-30 15:55:04 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from Bio import Phylo
-import matplotlib.pyplot as plt
-
-# Load the phylogenetic tree
-tree = Phylo.read("results/phylogenetic_tree.xml", "phyloxml")
-
-# Visualize the tree
-Phylo.draw(tree)
-plt.show()
